ef30c0d633fdc742fa1af8abe7c3454f6ce88c42
Adam Jaamour <adam@jamour.com>
2019-11-07 16:18:23 +0000

    Comment DisconnectionException class

--- src/exception/DisconnectionException.java
+++ src/exception/DisconnectionException.java
@@ -2,12 +2,17 @@ package exception;
 
 /**
  *
+ * Custom Exception class used to throw exceptions when a client unexpectedly disconnects from the server.
+ *
+ * @author 150014151
+ *
  */
 public class DisconnectionException extends Exception {
 
     /**
-     *
-     * @param message
+     * Constructor for the custom DisconnectionException.
+     * 
+     * @param message String message to display when a new exception is thrown.
      */
     public DisconnectionException(String message) {
         super(message);
