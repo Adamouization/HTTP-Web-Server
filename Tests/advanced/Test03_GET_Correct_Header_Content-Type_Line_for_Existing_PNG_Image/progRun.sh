690556b19ca6bc6765724e244a879f786ce3829c
Adam Jaamour <adam@jamour.com>
2019-11-08 19:58:23 +0000

    Fix test directory for advanced tests to be run locally

--- Tests/advanced/Test03_GET_Correct_Header_Content-Type_Line_for_Existing_PNG_Image/progRun.sh
+++ Tests/advanced/Test03_GET_Correct_Header_Content-Type_Line_for_Existing_PNG_Image/progRun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ROOT=$TESTDIR/../../../Resources/www
+ROOT=$TESTDIR/../../../www
 (timeout 2 java WebServerMain $ROOT 12345 > /dev/null 2>&1) & (sleep 1 ; curl -s -I -X GET localhost:12345/f1.png | grep -i -a 'Content-Type')
 wait
--- Tests/advanced/Test07_DELETE_Request_Should_Not_Return_Response_Body/progRun.sh
+++ Tests/advanced/Test07_DELETE_Request_Should_Not_Return_Response_Body/progRun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ROOT=$TESTDIR/../../../Resources/www
+ROOT=$TESTDIR/../../../www
 (timeout 2 java WebServerMain $ROOT 12345 > /dev/null 2>&1 ) & (sleep 1 ; curl -s -X DELETE localhost:12345/delete_me1.txt )
 wait
