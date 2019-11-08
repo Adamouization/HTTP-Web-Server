830d16b40ba70d11f9b9d918e7874eee6f19bf76
Adam Jaamour <adam@jamour.com>
2019-11-08 20:22:59 +0000

    Add note to advanced test 6

--- Tests/advanced/Test06_DELETE_Correct_Header_Status_Line_for_Existing_Text_File/progRun.sh
+++ Tests/advanced/Test06_DELETE_Correct_Header_Status_Line_for_Existing_Text_File/progRun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ROOT=$TESTDIR/../../../www
+# Note: delete_me.txt must exist, otherwise test will fail
 (timeout 2 java WebServerMain $ROOT 12345 > /dev/null 2>&1 ) & (sleep 1 ; curl -s -I -X DELETE localhost:12345/delete_me.txt | grep -i -a 'HTTP/1.1')
 wait
